06f62c0928d1e30f8619770d844d2c0d48079d6a
milamocha <d00253640@student.dkit.ie>
2024-05-02 17:20:02 +0100

    added style to posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,14 +1,19 @@
 @extends('layouts.app')
 @section('content')
-
     <div class="postsContainer">
         @foreach ($posts as $post)
             <div class="post">
-                <p>Posted by User ID: {{ $post->user_id }}</p>
-                <img src="{{ $post->image_path }}" alt="{{ $post->caption }}">
+                <div class="posttop">
+                    <img src="/images/catsleep.png">
+                    <p>{{ $post->user->username }}</p>
+                </div>
+                <!--<img src="{{ $post->image_path }}" alt="{{ $post->caption }}">-->
+                <div class="postimage">
+                    <img src="/images/catsleep.png">
+                </div>
                 <p>{{ $post->caption }}</hp>
                 <p>Created at: {{ $post->created_at }}</p>
             </div>
         @endforeach
     </div>
-    @endsection
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="/css/style.css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>WanderSnap</title>
 </head>
 
