f4c5992ce172fa8202bc6598166c686a6b224e01
milamocha <d00253640@student.dkit.ie>
2024-05-03 23:01:10 +0100

    Basic profile styling

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,44 +2,46 @@
 @section('content')
     @if (Auth::user())
         <div class="profileContainer">
-            <h1>User Profile</h1>
+            <div class="profileWrapper">
+                <div class="pfpContainer">
+                    <!--<img src="{{ $user->pfp ? asset('storage/' . $user->pfp) : '' }}" alt="{{ $user->name }}'s profile picture">-->
+                    <img src="/images/catsleep.png">
+                </div>
+                <div class="profileDetails">
+                    <p><b>{{ $user->username }}</b> <a href="{{ route('editprofile') }}">Edit Profile</a></p>
+                    <p>{{ $user->name }}</p>
+                    {{-- <p>Email: {{ $user->email }}</p> --}}
+                    <p>{{ $user->bio }}</p>
+                </div>
+            </div>
 
-            <h2>User Information</h2>
-            <p>Username: {{ $user->username }}</p>
-            <p>Name: {{ $user->name }}</p>
-            <p>Email: {{ $user->email }}</p>
-            <p>Bio: {{ $user->bio }}</p>
-            <img src="{{ $user->pfp ? asset('storage/' . $user->pfp) : '' }}" alt="{{ $user->name }}'s profile picture">
-
-        <a href="{{ route('editprofile') }}">Edit Profile</a>
-
-
-        <h2>Your Posts</h2>
-        @if (isset($posts) && count($posts))
-            <ul>
+            <h2>Your Posts</h2>
+            @if (isset($posts) && count($posts))
                 @foreach ($posts as $post)
-                    <li>
-                        <h3>{{ $post->caption }}</h3>
-
-                        @if ($post->user_id === Auth::id())
-                            <div class="d-flex gap-2">
-                                <a href="{{ route('posts.edit', $post) }}" class="btn btn-primary btn-sm">Edit</a>
-                                <form action="{{ route('posts.destroy', $post) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                                </form>
-                            </div>
-                        @endif
+                    @if ($post->user_id === Auth::id())
+                        <a href="{{ route('posts.edit', $post) }}" class="btn btn-primary btn-sm">Edit</a>
+                        <form action="{{ route('posts.destroy', $post) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Delete</button>
+                        </form>
+                    @endif
+                    <div class="post">
+                        <div class="posttop">
+                            <img src="/images/catsleep.png">
+                            <b>
+                                <p>{{ $post->user->username }}</p>
+                            </b>
+                        </div>
                         <div class="postimage">
                             <img src="{{ Storage::url($post->image_path) }}" alt="{{ $post->caption }}">
-                            <p>{{ $post->created_at->diffForHumans() }}</p>
                         </div>
-                    </li>
+                        <p>{{ $post->caption }}</hp>
+                        <p>{{ $post->created_at->diffForHumans() }}</p>
+                    </div>
                 @endforeach
-            </ul>
-        @else
-            <p>You have no posts yet.</p>
-        @endif
+            @else
+                <p>You have no posts yet.</p>
+            @endif
     @endif
 @endsection
