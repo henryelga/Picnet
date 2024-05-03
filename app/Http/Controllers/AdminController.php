06c06cbbbb1549493ee7a6a3604c5fa8caa5b2e1
milamocha <d00253640@student.dkit.ie>
2024-05-03 23:34:27 +0100

    Admins can view all users on admin page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AdminController extends Controller
 {
     public function dashboard()
     {
-        return view('admin.dashboard');
+        $users = User::all();
+
+        return view('admin.dashboard', ['users' => $users]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,26 @@
 @extends('layouts.app')
 @section('content')
-<h1>Test to see if admin page will work</h1>
+<div class="adminContainer">
+    <h1>Admin Dashboard</h1>
+    <h3>Users</h3>
+
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
