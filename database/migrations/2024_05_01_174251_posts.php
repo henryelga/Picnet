c0961600c4dc99adb10fa4be384569e73805ebbe
Elga J Henry <henryelga2004@gmail.com>
2024-05-04 14:26:09 +0100

    changes: not working

--- database/migrations/2024_05_01_174251_posts.php
+++ database/migrations/2024_05_01_174251_posts.php
@@ -4,20 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
-            $table->longText('caption');
-            $table->string('image_path');
-            $table->timestamps();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
-        });
+        //z
     }
 
     /**
@@ -25,6 +19,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        //
     }
 };
