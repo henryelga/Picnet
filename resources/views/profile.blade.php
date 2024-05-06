740fc03596d1bae844bbe6ed2663c9d3fbe29016
Elga J Henry <henryelga2004@gmail.com>
2024-05-06 22:37:44 +0100

    Updated user's profile pic

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -28,7 +28,9 @@
                     @endif
                     <div class="post">
                         <div class="posttop">
-                            <img src="/images/catsleep.png">
+                            {{-- <img src="/images/catsleep.png"> --}}
+                            <img src="{{ $post->user->pfp ? asset('storage/' . $post->user->pfp) : '' }}"
+                                alt="{{ $post->user->username }}'s profile picture">
                             <b>
                                 <p>{{ $post->user->username }}</p>
                             </b>
@@ -59,3 +61,60 @@
             @endif
     @endif
 @endsection
+@push('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.like-btn').click(function(e) {
+                e.preventDefault();
+                var postId = $(this).data('post-id');
+                var $likeBtn = $(this);
+                var $likeCount = $likeBtn.siblings('.like-count');
+                var $likeIcon = $likeBtn.find('.like-icon');
+
+                // Check if the button has the "heart-filled" class
+                if ($likeIcon.attr('src') === '{{ asset('images/red-heart.png') }}') {
+                    // Unlike the post
+                    $.ajax({
+                        url: '/posts/' + postId + '/unlike',
+                        method: 'DELETE',
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+                        success: function(response) {
+                            $likeIcon.attr('src', '{{ asset('images/heart.png') }}');
+                            $likeIcon.attr('alt', 'Not Liked');
+                            $likeCount.text(response.likeCount);
+                        },
+                        error: function(xhr, status, error) {
+                            console.error(error);
+                            alert(
+                                'An error occurred while processing your request. Please try again later.'
+                            );
+                        }
+                    });
+                } else {
+                    // Like the post
+                    $.ajax({
+                        url: '/posts/' + postId + '/like',
+                        method: 'POST',
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+                        success: function(response) {
+                            $likeIcon.attr('src', '{{ asset('images/red-heart.png') }}');
+                            $likeIcon.attr('alt', 'Liked');
+                            $likeCount.text(response.likeCount);
+                        },
+                        // error: function(xhr, status, error) {
+                        //     console.error(error);
+                        //     alert(
+                        //         'An error occurred while processing your request. '
+                        //     );
+                        // }
+                    });
+                }
+            });
+        });
+    </script>
+@endpush
