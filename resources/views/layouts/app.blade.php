c583ef35b8bb5b7ed124913e2140d9f1edb4a4f5
milamocha <d00253640@student.dkit.ie>
2024-05-07 20:41:46 +0100

    Styled profile page: made edit and delete buttons into drop downs on post and fixed aligning, styled buttons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- <title>{{ config('app.name', 'Laravel') }}</title> --}}
-    <title>WanderSnap</title>
+    <title>Picnet</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,12 +14,12 @@
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
 
-    <title>WanderSnap</title>
+    <title>Picnet</title>
 </head>
 
 <body>
     <div class="welcomeContainer">
-        <h1>WanderSnap</h1>
+        <h1>Picnet</h1>
         <div class="buttonContainer">
             <div class="reglogcontainer">
                 <a class="registerbutton" href="{{ route('register') }}">Register</a>
