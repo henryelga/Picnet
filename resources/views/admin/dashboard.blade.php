4086bea1252105c9f352d7684d72445c2846fa5a
milamocha <d00253640@student.dkit.ie>
2024-05-13 00:16:32 +0100

    Added username to admin dashboard table and styled table

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,14 +8,17 @@
             <thead>
                 <tr>
                     <th>ID</th>
+                    <th>Username</th>
                     <th>Name</th>
                     <th>Email</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($users as $user)
                     <tr>
                         <td>{{ $user->id }}</td>
+                        <td>{{ $user->username }}</td>
                         <td>{{ $user->name }}</td>
                         <td>{{ $user->email }}</td>
                         <td>
