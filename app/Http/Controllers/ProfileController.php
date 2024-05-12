11bff9cce33dedcc8bb89371cbf39c7d26c12954
milamocha <d00253640@student.dkit.ie>
2024-05-13 00:58:42 +0100

    Fixed error with "username already taken" for own usernames when updating profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -41,7 +41,7 @@ class ProfileController extends Controller
         $user = Auth::user();
 
         $validated = $request->validate([
-            'username' => 'required|string|max:255|unique:users,username',
+            'username' => 'required|string|max:255|unique:users,username,' . $user->id,
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:users,email,' . $user->id,
             'bio' => 'nullable|string',
